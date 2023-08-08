9f430183e737cd5dc3013534fb217d5f974ac54c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-08 21:36:36 +0300

    Admin group routes

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,27 @@ Route::prefix('user')->group(function () {
     Route::put('posts/{post}/like', [PostController::class, 'like'])->name('user.posts.like');
 });
 
+
+// Admin
+Route::prefix('admin')->group(function () {
+    // CRUD - Create, Read, Update, Delete
+    Route::get('/posts', [PostController::class, 'index'])->name('admin.posts');
+
+    Route::get('/posts/create', [PostController::class, 'create'])->name('admin.posts.create');
+
+    Route::post('/posts', [PostController::class, 'store'])->name('admin.posts.store');
+
+    Route::get('posts/{post}', [PostController::class, 'show'])->whereNumber('post')->name('admin.posts.show');
+
+    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('admin.posts.edit');
+
+    Route::put('posts/{post}', [PostController::class, 'update'])->name('admin.posts.update');
+
+    Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('admin.posts.destroy');
+
+    Route::put('posts/{post}/like', [PostController::class, 'like'])->name('admin.posts.like');
+});
+
 // Nested routes
 Route::resource('posts/{post}/comments', CommentController::class)->except([
     'index',
