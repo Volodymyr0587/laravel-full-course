b91446185187348e14ed86cc7f4d53fed234344c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-10 19:06:59 +0300

    create components for header, footer

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,20 +9,19 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
 
-    <div class="flex flex-col justify-between min-h-screen text-center">
-        <header>
-            Header
-        </header>
+    <div class="flex flex-col items-center justify-between min-h-screen text-center">
+        @include('components.header')
 
-        <main class="flex-grow">
-            Content
+        <main class="flex grow">
+            Main Content
         </main>
 
-        <footer>
-            Footer
-        </footer>
+        @include('components.footer')
+
     </div>
 </body>
+
 </html>
