1e0d004af77f802890b40eb2f77cc4e4f8e2328b
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-11 13:06:19 +0300

    Register page

---
+++ resources/views/components/register.blade.php
@@ -0,0 +1,3 @@
+<main class="flex grow py-3">
+    <h1 class="text-xl font-bold">Register page</h1>
+</main>
---
+++ resources/views/register/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layout')
+
+@section('page.title')
+    Register Page
+@endsection
+
+@section('content')
+
+    <x-register />
+
+@endsection
+
