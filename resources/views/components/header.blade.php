1606cab4297634d9af3909608a13fd20a94bb82d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-11 14:14:43 +0300

    links color

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,17 +2,17 @@
     <div class="container">
         <div class="flex justify-between">
             <div class="px-4">
-                <a href="{{ route('home') }}">
+                <a href="{{ route('home') }}" class="font-bold text-emerald-900 hover:text-cyan-400">
                     {{ config('app.name') }}
                 </a>
             </div>
             <div>
                 <ul class="flex space-x-4">
                     <li>
-                        <a href="{{ route('register') }}">Registration</a>
+                        <a href="{{ route('register') }}" class="font-bold text-emerald-900 hover:text-cyan-400">Registration</a>
                     </li>
                     <li>
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}" class="font-bold text-emerald-900 hover:text-cyan-400">Login</a>
                     </li>
                 </ul>
             </div>
