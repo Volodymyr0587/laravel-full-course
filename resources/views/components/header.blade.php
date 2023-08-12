58324cb3b29334366b9a65124a3f720cc101c29b
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-12 15:22:13 +0300

    __ function for locale

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,11 +5,13 @@
                 <ul class="flex space-x-4">
                     <li>
                         <a href="{{ route('home') }}" class="{{ request()->is('/') ? 'bg-cyan-400 font-bold text-emerald-900 p-2 rounded-2xl' : 'font-bold text-emerald-900 hover:text-cyan-400' }}">
-                            {{ config('app.name') }}
+                            {{ __(config('app.name')) }}
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route('blog') }}" class="{{ request()->is('blog') ? 'bg-cyan-400 font-bold text-emerald-900 p-2 rounded-2xl' : 'font-bold text-emerald-900 hover:text-cyan-400' }}">Posts</a>
+                        <a href="{{ route('blog') }}" class="{{ request()->is('blog') ? 'bg-cyan-400 font-bold text-emerald-900 p-2 rounded-2xl' : 'font-bold text-emerald-900 hover:text-cyan-400' }}">
+                            {{ __('Posts') }}
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -17,11 +19,15 @@
                 <ul class="flex space-x-4">
                     <li>
                         <a href="{{ route('register') }}"
-                            class="{{ request()->is('register') ? 'bg-cyan-400 font-bold text-emerald-900 p-2 rounded-2xl' : 'font-bold text-emerald-900 hover:text-cyan-400' }}">Registration</a>
+                            class="{{ request()->is('register') ? 'bg-cyan-400 font-bold text-emerald-900 p-2 rounded-2xl' : 'font-bold text-emerald-900 hover:text-cyan-400' }}">
+                            {{ __('Registration') }}
+                        </a>
                     </li>
                     <li>
                         <a href="{{ route('login') }}"
-                            class="{{ request()->is('login') ? 'bg-cyan-400 font-bold text-emerald-900 p-2 rounded-2xl' : 'font-bold text-emerald-900 hover:text-cyan-400' }}">Login</a>
+                            class="{{ request()->is('login') ? 'bg-cyan-400 font-bold text-emerald-900 p-2 rounded-2xl' : 'font-bold text-emerald-900 hover:text-cyan-400' }}">
+                            {{ __('Login') }}
+                        </a>
                     </li>
                 </ul>
             </div>
