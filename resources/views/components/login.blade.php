8b72c21c2ea19105b8171eb8d41c9e1f4fd79740
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-13 15:54:34 +0300

    form components refactiring

--- resources/views/components/login.blade.php
+++ resources/views/components/login.blade.php
@@ -2,41 +2,41 @@
 
 
     <div>
-        <x-form-header>
+        <x-form.form-header>
             {{ __('Login') }}
-        </x-form-header>
+        </x-form.form-header>
 
-        <x-form action="{{ route('login.store') }}" method="POST">
+        <x-form.form action="{{ route('login.store') }}" method="POST">
 
-            <x-form-input-block>
-                <x-form-label for="email">
+            <x-form.form-input-block>
+                <x-form.form-label for="email">
                     {{ __('Email') }}
-                </x-form-label>
-                <x-form-input name="email" id="email" type="email" placeholder="Email" autofocus />
-            </x-form-input-block>
+                </x-form.form-label>
+                <x-form.form-input name="email" id="email" type="email" placeholder="Email" autofocus />
+            </x-form.form-input-block>
 
-            <x-form-input-block>
-                <x-form-label for="password">
+            <x-form.form-input-block>
+                <x-form.form-label for="password">
                     {{ __('Password') }}
-                </x-form-label>
-                <x-form-input name="password" id="password" type="password" placeholder="******************" />
+                </x-form.form-label>
+                <x-form.form-input name="password" id="password" type="password" placeholder="******************" />
                 {{-- <p class="text-red-500 text-xs italic">Please choose a password.</p> --}}
-            </x-form-input-block>
+            </x-form.form-input-block>
 
-            <x-remember-me name="remember" id="remember" type="checkbox" value="">
+            <x-form.remember-me name="remember" id="remember" type="checkbox" value="">
                 {{ __('Remeber Me') }}
-            </x-remember-me>
+            </x-form.remember-me>
 
-            <x-form-buttons-block>
+            <x-form.form-buttons-block>
                 <x-button type="submit" class="">
                     {{ __('Sign In') }}
                 </x-button>
-                <x-form-login-register-link href="{{ route('register') }}">
+                <x-form.form-login-register-link href="{{ route('register') }}">
                     {{ __('Register') }}
-                </x-form-login-register-link>
-            </x-form-buttons-block>
+                </x-form.form-login-register-link>
+            </x-form.form-buttons-block>
 
-        </x-form>
+        </x-form.form>
 
     </div>
 
--- resources/views/components/register.blade.php
+++ resources/views/components/register.blade.php
@@ -1,60 +1,60 @@
 <main class="flex grow py-3">
 
     <div>
-        <x-form-header>
+        <x-form.form-header>
             {{ __('Register') }}
-        </x-form-header>
+        </x-form.form-header>
 
-        <x-form action="{{ route('register.store') }}" method="POST">
+        <x-form.form action="{{ route('register.store') }}" method="POST">
 
-            <x-form-input-block>
-                <x-form-label for="email">
+            <x-form.form-input-block>
+                <x-form.form-label for="email">
                     {{ __('Email') }}
-                </x-form-label>
-                <x-form-input name="email" id="email" type="email" placeholder="Email" autofocus></x-form-input>
-            </x-form-input-block>
+                </x-form.form-label>
+                <x-form.form-input name="email" id="email" type="email" placeholder="Email" autofocus></x-form.form-input>
+            </x-form.form-input-block>
 
-            <x-form-input-block>
-                <x-form-label for="name">
+            <x-form.form-input-block>
+                <x-form.form-label for="name">
                     {{ __('Name') }}
-                </x-form-label>
-                <x-form-input name="name" id="name" type="text" placeholder="Name"></x-form-input>
-            </x-form-input-block>
+                </x-form.form-label>
+                <x-form.form-input name="name" id="name" type="text" placeholder="Name"></x-form.form-input>
+            </x-form.form-input-block>
 
-            <x-form-input-block>
-                <x-form-label for="password">
+            <x-form.form-input-block>
+                <x-form.form-label for="password">
                     {{ __('Password') }}
-                </x-form-label>
-                <x-form-input name="password" id="password" type="password"
-                    placeholder="******************"></x-form-input>
+                </x-form.form-label>
+                <x-form.form-input name="password" id="password" type="password"
+                    placeholder="******************"></x-form.form-input>
                 {{-- <p class="text-red-500 text-xs italic">Please choose a password.</p> --}}
-            </x-form-input-block>
+            </x-form.form-input-block>
 
-            <x-form-input-block>
-                <x-form-label for="password">
+            <x-form.form-input-block>
+                <x-form.form-label for="password">
                     {{ __('Confirm password') }}
-                </x-form-label>
-                <x-form-input name="password_confirmation" id="password_confirmation" type="password"
-                    placeholder="******************"></x-form-input>
+                </x-form.form-label>
+                <x-form.form-input name="password_confirmation" id="password_confirmation" type="password"
+                    placeholder="******************"></x-form.form-input>
                 {{-- <p class="text-red-500 text-xs italic">Please choose a password.</p> --}}
-            </x-form-input-block>
+            </x-form.form-input-block>
 
-            <x-remember-me name="agreement" id="agreement" type="checkbox" value="">
+            <x-form.remember-me name="agreement" id="agreement" type="checkbox" value="">
                 {{ __('I agree to the processing of personal data') }}
-            </x-remember-me>
+            </x-form.remember-me>
 
-            <x-form-buttons-block>
+            <x-form.form-buttons-block>
                 <x-button type="submit" class="">
                     {{ __('Register') }}
                 </x-button>
-                <x-form-login-register-link href="{{ route('login') }}">
+                <x-form.form-login-register-link href="{{ route('login') }}">
                     {{ __('Login') }}
-                </x-form-login-register-link>
-            </x-form-buttons-block>
+                </x-form.form-login-register-link>
+            </x-form.form-buttons-block>
             {{-- <x-slot name="right">
                 {{ __('X-SLOT') }}
             </x-slot> --}}
-        </x-form>
+        </x-form.form>
 
     </div>
 
