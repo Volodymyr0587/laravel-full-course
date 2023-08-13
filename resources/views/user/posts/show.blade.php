be11d677f68e0b1df9e543dfa77b0c8fd6f9ec10
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-13 18:21:59 +0300

    Back link from single user post to list of user posts

--- resources/views/user/posts/show.blade.php
+++ resources/views/user/posts/show.blade.php
@@ -4,13 +4,14 @@
 
 @section('content')
     <main class="py-3">
-        <x-title>{{ __('View Post') }}</x-title>
+        <x-title>{{ __('View Post') }}
+            <div class="m-0 p-0">
+                <a href="{{ route('user.posts') }}" class="text-sm text-blue-500">< - Back</a>
+            </div>
+        </x-title>
 
         <div class="mt-10">
-
             <div class="mb-4">
-
-
                 <h4 class="text-xl">{{ $post->title }}</h4>
 
                 <div class="flex space-x-2 ml-10">
