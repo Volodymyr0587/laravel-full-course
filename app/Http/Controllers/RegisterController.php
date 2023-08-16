2152be726b670aaa804a50a951903fb4d81023c6
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-16 17:16:06 +0300

    request methods and parameters

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,8 +11,22 @@ class RegisterController extends Controller
         return view('register.index');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        // $data = $request->all();
+        // $data = $request->only(['name', 'password']);
+        // $data = $request->except(['name', 'password']);
+        // dd($data);
+
+        $name = $request->input('name');
+        // $email = $request->email;
+        $email = $request->input('email');
+
+        // $agreement = !! $request->agreement;
+        $agreement = $request->boolean('agreement');
+
+        dd($name, $email, $agreement);
+
         return "Register new user";
     }
 }
--- resources/views/components/form/remember-me.blade.php
+++ resources/views/components/form/remember-me.blade.php
@@ -2,7 +2,7 @@
     <input
     {{ $attributes->merge(['class' => 'w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600']) }}
     >
-    <label class="ml-2 text-gray-700 text-sm font-bold" for="remember">
+    <label class="ml-2 text-gray-700 text-sm font-bold" for="agreement">
         {{ $slot }}
     </label>
 </div>
