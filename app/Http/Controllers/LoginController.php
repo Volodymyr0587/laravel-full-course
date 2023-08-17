7db717fed65e808dede40c35d1b3cfb92dd761e2
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-17 18:28:00 +0300

    request->ip() request->path() request->url() request->is() request->routeIs()

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,6 +20,14 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
+        // $ip = $request->ip();
+        // $path = $request->path();
+        // $url = $request->url();
+        // dd($ip, $path, $url);
+
+        // dd($request->is('log*'));
+        // dd($request->routeIs('log*'));
+
         $email = $request->email;
         $password = $request->password;
         $remember = $request->boolean('remember');
