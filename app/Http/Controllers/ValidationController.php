25fbbeb7e31a2458ccc72c606c679be2df14a065
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-25 18:51:57 +0300

    Using Closures for validation

--- app/Http/Controllers/ValidationController.php
+++ app/Http/Controllers/ValidationController.php
@@ -25,11 +25,11 @@ class ValidationController extends Controller
             'password' => ['required', 'string', 'min:7', 'confirmed'], // pasword_confirmation
             'password_confirmation' => ['required', 'string', 'min:7'],
             'current_password' => ['required', 'string', 'current_password'], // current password
-            'email' => ['required', 'string', 'email', 'exists:user,email'], // mail@example.com
+            'email' => ['required', 'string', 'email', 'max:100', 'exists:user,email'], // mail@example.com
             'country_id' => ['required', 'integer', 'exists:coutries,id'],
             // 'country_id' => ['required', 'integer', Rule::exists('countries', 'id')->where('active', true)],
             // 'phone' => ['required', 'string', 'unique:users,phone'],
-            'phone' => ['required', 'string', 'digits_between:10,20', new Phone, Rule::unique('users', 'phone')->ignore($user)],
+            'phone' => ['required', 'string', new Phone, Rule::unique('users', 'phone')->ignore($user)],
             'website' => ['nullable', 'string', 'url'], // https:://example.com
             'uuid' => ['nullable', 'string', 'uuid'], //sdfsdf-sdfsdf-sdfsdf-sdfsdf
             'ip' => ['nullable', 'string', 'ip'], // 127.0.0.1
@@ -37,7 +37,16 @@ class ValidationController extends Controller
             'birth_date' => ['nullable', 'string', 'date'], // 2023-08-25 / 25-08-2023 17:07:07
             'start_date' => ['required', 'string', 'date', 'after_or_equal:today'],
             'finish_date' => ['required', 'string', 'date', 'after:start_date'],
-
+            'services' => ['nullable', 'array', 'min:1', 'max:10'], // [1, 2, 3, 4, 5]
+            'services.*' => ['required', 'integer'], // [1, 2, 3, 4, 5]
+            'delivery' => ['required', 'array', 'size:2'], // ['date' => '2023-08-25', 'time' => '12:30:00']
+            'delivery.date' => ['required', 'string', 'date_format:Y.m.d'], // 2021-10-09
+            'delivery.time' => ['required', 'string', 'date_format:H:i:s'], // 12:30:00
+            'secret' => ['required', 'string', function ($attribute, $value, \Closure $fail) {
+                if ($value !== config('example.secret')) {
+                    $fail(__('Wrong Secret Key.'));
+                }
+            }],
 
         ]);
 
