40aa2f96d01ee117dde0a5ac4392ebe0ca02d4e6
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-09-04 18:43:14 +0300

    Raw sql query

--- app/Http/Controllers/User/DonateController.php
+++ app/Http/Controllers/User/DonateController.php
@@ -29,13 +29,24 @@ class DonateController extends Controller
         // //* Максимальне значення
         // $max_amount = Donate::query()->max('amount');
 
-        $stats = [
-            'total_count' => Donate::query()->count(),
-            'total_amount' => Donate::query()->sum('amount'),
-            'avg_amount' => Donate::query()->avg('amount'),
-            'min_amount' => Donate::query()->min('amount'),
-            'max_amount' => Donate::query()->max('amount'),
-        ];
+        // $stats = [
+        //     'total_count' => Donate::query()->count(),
+        //     'total_amount' => Donate::query()->sum('amount'),
+        //     'avg_amount' => Donate::query()->avg('amount'),
+        //     'min_amount' => Donate::query()->min('amount'),
+        //     'max_amount' => Donate::query()->max('amount'),
+        // ];
+
+        $stats = Donate::query()
+            ->selectRaw('count(*) as total_count')
+            ->selectRaw('sum(amount) as total_amount')
+            ->selectRaw('avg(amount) as avg_amount')
+            ->selectRaw('min(amount) as min_amount')
+            ->selectRaw('max(amount) as max_amount')
+            ->first();
+
+        // dd($stats->toArray());
+
         return view('user.donates.index', compact('stats'));
     }
 }
