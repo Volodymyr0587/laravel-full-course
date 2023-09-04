26f5dea8161c8aa6664bf38993fec6adf1ad2d01
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-09-04 17:05:16 +0300

    Create data using Factory and tinker

--- app/Http/Controllers/User/DonateController.php
+++ app/Http/Controllers/User/DonateController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Donate;
 use Illuminate\Http\Request;
 
 class DonateController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        $stats = Donate::query()->count(); //*кількість записів в таблиці `donates`
+
+        dd($stats);
+    }
 }
--- app/Models/Donate.php
+++ app/Models/Donate.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Donate extends Model
 {
+    use HasFactory;
     protected $fillable = [
+        // 'created_at',
         'amount'
     ];
 }
